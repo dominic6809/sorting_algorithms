9f3ea84dd7f622da877305311f8aa444bca779b3
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-30 11:05:27 +0300

    sort.h header file

--- sort.h
+++ sort.h
@@ -3,9 +3,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stddef.h>
+#include <string.h>
 #include <stdbool.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -21,22 +36,22 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);
+/* Printing helper functions */
 void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void counting_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void recursive_quick_sort(int *array, int lowest, int highest, size_t size);
-int lomuto_partition(int *array, int lowest, int highest, size_t size);
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void merge(int *array, int *left, size_t left_size,
-		int *right, size_t right_size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
