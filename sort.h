9ff2fe131e1981927d01e9466e1d3c95104c4d6b
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-30 11:09:53 +0300

    sort.h header file

--- sort.h
+++ sort.h
@@ -16,11 +16,13 @@
  * @false: Equals 0.
  * @true: Equals 1.
  */
-typedef enum bool
-{
-	false = 0,
-	true
-} bool;
+/**
+* typedef enum bool
+* {
+* false = 0,
+* true
+* } bool;
+*/
 
 /**
  * struct listint_s - Doubly linked list node
