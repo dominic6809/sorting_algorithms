de1de8dbdf5cf9bcd32da23e028d509980429a61
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-04-30 10:17:49 +0300

    Sorts an array of integers in ascending order

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,51 +10,61 @@
  * sort the elements of the array in ascending order. It prints the counting
  * array once it is set up.
  */
+int get_max(int *array, int size)
+{
+	int max;
+	int i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * counting_sort - Sort an array of integers in ascending order
+ * using the counting sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array
+ * Description: Prints the counting array after setting it up.
+ */
 void counting_sort(int *array, size_t size)
 {
-	int *count;
-	int *sorted;
-	int x, max;
+	int *count, *sorted, max, i;
 
 	if (array == NULL || size < 2)
 		return;
 
-	/* Find the maximum value in the array */
-	max = array[0];
-
-	for (x = 1; x < (int)size; x++)
-	{
-	if (array[x] > max)
-	max = array[x];
-	}
-
-	count = malloc(sizeof(int) * (max + 1));
-	if (count == NULL)
-	return;
-
 	sorted = malloc(sizeof(int) * size);
 	if (sorted == NULL)
+		return;
+	max = get_max(array, size);
+	count = malloc(sizeof(int) * (max + 1));
+	if (count == NULL)
 	{
-	free(count);
-	return;
+		free(sorted);
+		return;
 	}
 
-	for (x = 0; x <= max; x++)
-	count[x] = 0;
-
-	for (x = 0; x < (int)size; x++)
-	count[array[x]] += 1;
-
+	for (i = 0; i < (max + 1); i++)
+		count[i] = 0;
+	for (i = 0; i < (int)size; i++)
+		count[array[i]] += 1;
+	for (i = 0; i < (max + 1); i++)
+		count[i] += count[i - 1];
 	print_array(count, max + 1);
 
-	for (x = 0; x < (int)size; x++)
+	for (i = 0; i < (int)size; i++)
 	{
-	sorted[count[array[x]] - 1] = array[x];
-	count[array[x]] -= 1;
+		sorted[count[array[i]] - 1] = array[i];
+		count[array[i]] -= 1;
 	}
 
-	for (x = 0; x < (int)size; x++)
-	array[x] = sorted[x];
+	for (i = 0; i < (int)size; i++)
+		array[i] = sorted[i];
 
 	free(sorted);
 	free(count);
