72a0e2f84fa7ad237dda7b889eeaa2be68060044
JentleJoe <aigbokhaebholojoshua@gmail.com>
2024-05-01 04:51:15 +0100

    updated header file

--- sort.h
+++ sort.h
@@ -55,5 +55,8 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void quicksort(int *array, int low, int high, size_t size);
+void swap(int *a, int *b);
+int lomuto_partition(int *array, int low, int high, size_t size);
 
 #endif /* SORT_H */
