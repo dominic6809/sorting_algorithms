3a922e4006167b741fe42085c6d85fd770c991b0
JentleJoe <aigbokhaebholojoshua@gmail.com>
2024-05-01 05:09:49 +0100

    updated header file

--- sort.h
+++ sort.h
@@ -58,5 +58,6 @@ void quick_sort_hoare(int *array, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
 int lomuto_partition(int *array, int low, int high, size_t size);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 
 #endif /* SORT_H */
