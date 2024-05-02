460a8ab064ebec33f744d1a13c43c3d50b3f21ae
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-05-02 11:31:37 +0300

    header file

--- sort.h
+++ sort.h
@@ -1,11 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <string.h>
-#include <stdbool.h>
+#include <stdio.h>
 
 /* Comparison direction macros for bitonic sort */
 #define UP 0
@@ -16,17 +13,14 @@
  * @false: Equals 0.
  * @true: Equals 1.
  */
-/**
-* typedef enum bool
-* {
-* false = 0,
-* true
-* } bool;
-*/
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
- *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
@@ -55,10 +49,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void quicksort(int *array, int low, int high, size_t size);
-void swap(int *a, int *b);
-int lomuto_partition(int *array, int low, int high, size_t size);
-void swap_nodes(listint_t *a, listint_t *b);
-void merge(int *array, int *tmp, size_t l, size_t m, size_t r);
 
 #endif /* SORT_H */
